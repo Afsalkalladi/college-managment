e0da1a4c0116bfad08b64569bc62804362ce9db0
Afsalkalladi <76440747+Afsalkalladi@users.noreply.github.com.com>
2025-05-23 15:26:49 +0530

    corrected student db

--- apps/students/apps.py
+++ apps/students/apps.py
@@ -1,15 +1,15 @@
-from django.apps import AppConfig
-
-class StudentsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.students'
-
 # students/apps.py
 from django.apps import AppConfig
 
+
 class StudentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.students'
+    name = 'apps.students'  # Use just 'students' if app is in root
+    # name = 'apps.students'  # Use this if app is inside apps/ folder
     
     def ready(self):
-        import students.signals
+        # Import signals when app is ready
+        try:
+            import students.signals
+        except ImportError:
+            pass  # Signals file doesn't exist yet
